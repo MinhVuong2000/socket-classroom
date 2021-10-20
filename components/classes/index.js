eef2530eff537be451bcc7b75736d03c287dff11
Vuong Nguyen Thi Minh <minhvuong160620@gmail.com>
2021-10-20 23:03:00 +0700

    Delete console.log()

--- components/classes/index.js
+++ components/classes/index.js
@@ -13,21 +13,17 @@ router.get('/', async function(req, res, next) {
 router.get('/detail/:id', async function(req, res, next) {
     const id = parseInt(req.params.id);
     const item = await db.one(id);
-    console.log(item);
     res.json(item);
 });
 
 router.post('/', async function(req, res, next){
-    console.log('post api')
     const new_class = {className: req.body.className};
-    console.log(new_class);
     await db.add(new_class);
     const allDB = await db.all();
     res.json(allDB);
 });
 
 router.get('/isExistedClassName', async function(req, res, next){
-    console.log('isExistedClassName api')
     const checked_name = req.query.checked_name;
     const isExisted = await db.isExisted(checked_name);
     res.json(isExisted);
