76eed7f9ce025bf396f5d216f38f0377bab41ac4
Vuong Nguyen Thi Minh <46895260+MinhVuong2000@users.noreply.github.com>
2022-01-17 14:16:55 +0700

    Change domain after hosting

--- config/const.config.js
+++ config/const.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     postsLimit: 6,
-    DOMAIN_API: 'http://localhost:3000/',
-    DOMAIN_FE: 'http://localhost:3001/',
-    // DOMAIN_API: 'https://classroom-grade-manager-api.herokuapp.com/',//backend
-    // DOMAIN_FE: 'https://classroom-grade-manager-fe.herokuapp.com/',//frontend
+//     DOMAIN_API: 'http://localhost:3000/',
+//     DOMAIN_FE: 'http://localhost:3001/',
+    DOMAIN_API: 'https://final-project-classroom-api.herokuapp.com/',//backend
+    DOMAIN_FE: 'https://final-project-classroom-fe.herokuapp.com/',//frontend
 }
